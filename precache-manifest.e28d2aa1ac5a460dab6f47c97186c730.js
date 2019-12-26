7f02a4135975edd061f2fa9ee5134b2328ca3298
x566chen <31770049+x566chen@users.noreply.github.com>
2019-12-26 08:38:05 +0000

    Deploying to master from  5c0aa011010a07ef1768b493f0c1c258572b5087

---
+++ precache-manifest.e28d2aa1ac5a460dab6f47c97186c730.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: '8040801fbb9e3311ce31afc38c8d3448',
+    url: './index.html',
+  },
+  {
+    revision: 'dcbef487ab090f984113',
+    url: './static/css/main.e9f78689.chunk.css',
+  },
+  {
+    revision: '261b73bdcf7a71d182f7',
+    url: './static/js/2.b49bab47.chunk.js',
+  },
+  {
+    revision: 'b51e62edbcf7a9c4f4ad13f506021d3e',
+    url: './static/js/2.b49bab47.chunk.js.LICENSE',
+  },
+  {
+    revision: 'dcbef487ab090f984113',
+    url: './static/js/main.b20a32db.chunk.js',
+  },
+  {
+    revision: '75c35823b6c1c582671d',
+    url: './static/js/runtime-main.f4662f19.js',
+  },
+]);

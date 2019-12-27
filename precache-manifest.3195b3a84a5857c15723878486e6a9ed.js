42bf27e1a1ef679d14256b08377b0ac118d2f7f1
yuhao3570 <45909402+yuhao3570@users.noreply.github.com>
2019-12-27 06:50:53 +0000

    Deploying to gh-pages from  ac52bf456a1c1d487ec793b54075c2f501161543

---
+++ precache-manifest.3195b3a84a5857c15723878486e6a9ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d44928655a5daa04234e389e9d64e254",
+    "url": "./index.html"
+  },
+  {
+    "revision": "05b153f6904ecc54f04b",
+    "url": "./static/css/main.e99b11a2.chunk.css"
+  },
+  {
+    "revision": "2fb6c33f65843768b80d",
+    "url": "./static/js/2.b0b708e5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "./static/js/2.b0b708e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05b153f6904ecc54f04b",
+    "url": "./static/js/main.3d3a96cb.chunk.js"
+  },
+  {
+    "revision": "75c35823b6c1c582671d",
+    "url": "./static/js/runtime-main.f4662f19.js"
+  },
+  {
+    "revision": "7a7930a26ffe78ed1c7375364bde24c2",
+    "url": "./static/media/click1.7a7930a2.wav"
+  }
+]);

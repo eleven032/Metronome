f32087b898c0e17411d46763febfbc95a05407b6
cathy <x566chen@uwaterloo.ca>
2019-12-27 14:23:22 +0800

    modify bmpslider.js

--- src/components/BPMSlider.js
+++ src/components/BPMSlider.js
@@ -3,7 +3,6 @@ import Slider from '@material-ui/core/Slider';
 import PropTypes from 'prop-types';
 
 export default function BPMSlider({ value, setBPM }) {
-  localStorage.setItem('bpm', value);
   return (
     <Slider
       track="normal"
@@ -12,7 +11,10 @@ export default function BPMSlider({ value, setBPM }) {
       step={5}
       min={60}
       max={240}
-      onChange={(_, sliderValue) => { setBPM(sliderValue); }}
+      onChange={(_, sliderValue) => {
+        setBPM(sliderValue);
+        localStorage.setItem('bpm', sliderValue);
+      }}
     />
   );
 }
